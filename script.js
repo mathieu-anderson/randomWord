664eaec49ced6a91026c463ba53926b7297b6a17
Mathieu <mathieu.hj.anderson@gmail.com>
2016-11-21 11:42:42 +1300

    Add random cell selector, does not work

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 $(document).ready(function () {
 
-var randomWord = function(){
+var randID = function(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+$(".cell").click(function(){
+  var randDiv = randID(0,8);
+  var cells = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+  var displayCell = '"#'+cells[randDiv]+'"'; //get a random ID in the "#ID" format
     $.ajax({
         type: "GET",
         url: "http://www.setgetgo.com/randomword/get.php?len=8",
         dataType: "jsonp",
         jsonpCallback: "randomWord",
         success: function(data) {
-        $("#one").html(data["Word"]);
+         $(displayCell).html(data.Word); // why doesn't it work?
         }
     });
-}
-
-randomWord();
+});
 
 
 })
