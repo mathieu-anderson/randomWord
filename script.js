61333c35fd4a4c38d3f78964e3eda0d60fb5d1df
Mathieu <mathieu.hj.anderson@gmail.com>
2016-11-21 14:16:11 +1300

    Add random word, random cell, pop sound.

--- script.js
+++ script.js
@@ -5,20 +5,40 @@ var randID = function(min,max)
     return Math.floor(Math.random()*(max-min+1)+min);
 }
 
+$.ajax({
+    type: "GET",
+    url: "http://www.setgetgo.com/randomword/get.php?len=8",
+    dataType: "jsonp",
+    jsonpCallback: "randomWord",
+    success: function(data) {
+      var randDiv = randID(0,8);
+      var cells = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+      var displayCell = '#'+cells[randDiv];
+    $(displayCell).addClass("target").html(data.Word);
+   }
+ })
+
 $(".cell").click(function(){
   var randDiv = randID(0,8);
   var cells = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-  var displayCell = '"#'+cells[randDiv]+'"'; //get a random ID in the "#ID" format
+  var displayCell = '#'+cells[randDiv];
     $.ajax({
         type: "GET",
         url: "http://www.setgetgo.com/randomword/get.php?len=8",
         dataType: "jsonp",
         jsonpCallback: "randomWord",
         success: function(data) {
-         $(displayCell).html(data.Word); // why doesn't it work?
+         $(".cell").empty();
+         $(displayCell).addClass("target").html(data.Word);
         }
     });
+    var soundPop = function() {
+        var audioPop = document.getElementById("pop");
+        audioPop.play();
+    }
+      soundPop();
 });
+
 
 
 })
