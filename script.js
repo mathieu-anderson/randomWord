071175c8aaac97f174c06fa0b4479416e8808f9a
Mathieu <mathieu.hj.anderson@gmail.com>
2016-11-21 15:50:15 +1300

    Finalized .target behaviour

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
 $(document).ready(function() {
 
-    var randID = function(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) + min);
-    }
+  var randID = function(min, max) {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+    $(document).on("click", ".target", function() {
+      var randDiv = randID(0, 8);
+            var cells = [
+                "one",
+                "two",
+                "three",
+                "four",
+                "five",
+                "six",
+                "seven",
+                "eight",
+                "nine"
+            ];
+            var displayCell = '#' + cells[randDiv];
+            $.ajax({
+                type: "GET",
+                url: "http://www.setgetgo.com/randomword/get.php",
+                dataType: "jsonp",
+                jsonpCallback: "randomWord",
+                success: function(data) {
+                    $(".cell").removeClass("target");
+                    $(".cell").empty();
+                    $(displayCell).addClass("target").html(data.Word);
+                }
+            });
+            var soundPop = function() {
+                var audioPop = document.getElementById("pop");
+                audioPop.play();
+            }
+            soundPop();
+        });
 
     $.ajax({
         type: "GET",
@@ -26,37 +58,5 @@ $(document).ready(function() {
             $(displayCell).addClass("target").html(data.Word);
         }
     })
-
-    $(".cell").click(function() {
-        var randDiv = randID(0, 8);
-        var cells = [
-            "one",
-            "two",
-            "three",
-            "four",
-            "five",
-            "six",
-            "seven",
-            "eight",
-            "nine"
-        ];
-        var displayCell = '#' + cells[randDiv];
-        $.ajax({
-            type: "GET",
-            url: "http://www.setgetgo.com/randomword/get.php?len=8",
-            dataType: "jsonp",
-            jsonpCallback: "randomWord",
-            success: function(data) {
-                $(".cell").removeClass("target");
-                $(".cell").empty();
-                $(displayCell).addClass("target").html(data.Word);
-            }
-        });
-        var soundPop = function() {
-            var audioPop = document.getElementById("pop");
-            audioPop.play();
-        }
-        soundPop();
-    });
 
 })
