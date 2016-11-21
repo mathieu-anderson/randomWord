8c13c504e8c1053c8bdc8bfb2f4dde3e4fd8aca4
Mathieu <mathieu.hj.anderson@gmail.com>
2016-11-21 14:36:35 +1300

    Fixed .target attribution

--- script.js
+++ script.js
@@ -1,44 +1,62 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-var randID = function(min,max)
-{
-    return Math.floor(Math.random()*(max-min+1)+min);
-}
+    var randID = function(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
 
-$.ajax({
-    type: "GET",
-    url: "http://www.setgetgo.com/randomword/get.php?len=8",
-    dataType: "jsonp",
-    jsonpCallback: "randomWord",
-    success: function(data) {
-      var randDiv = randID(0,8);
-      var cells = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-      var displayCell = '#'+cells[randDiv];
-    $(displayCell).addClass("target").html(data.Word);
-   }
- })
-
-$(".cell").click(function(){
-  var randDiv = randID(0,8);
-  var cells = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-  var displayCell = '#'+cells[randDiv];
     $.ajax({
         type: "GET",
         url: "http://www.setgetgo.com/randomword/get.php?len=8",
         dataType: "jsonp",
         jsonpCallback: "randomWord",
         success: function(data) {
-         $(".cell").empty();
-         $(displayCell).addClass("target").html(data.Word);
+            var randDiv = randID(0, 8);
+            var cells = [
+                "one",
+                "two",
+                "three",
+                "four",
+                "five",
+                "six",
+                "seven",
+                "eight",
+                "nine"
+            ];
+            var displayCell = '#' + cells[randDiv];
+            $(displayCell).addClass("target").html(data.Word);
         }
+    })
+
+    $(".cell").click(function() {
+        var randDiv = randID(0, 8);
+        var cells = [
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine"
+        ];
+        var displayCell = '#' + cells[randDiv];
+        $.ajax({
+            type: "GET",
+            url: "http://www.setgetgo.com/randomword/get.php?len=8",
+            dataType: "jsonp",
+            jsonpCallback: "randomWord",
+            success: function(data) {
+                $(".cell").removeClass("target");
+                $(".cell").empty();
+                $(displayCell).addClass("target").html(data.Word);
+            }
+        });
+        var soundPop = function() {
+            var audioPop = document.getElementById("pop");
+            audioPop.play();
+        }
+        soundPop();
     });
-    var soundPop = function() {
-        var audioPop = document.getElementById("pop");
-        audioPop.play();
-    }
-      soundPop();
-});
-
-
 
 })
